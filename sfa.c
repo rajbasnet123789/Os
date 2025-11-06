7883287543e3de2ca905c5a577be6a63be9511f9
rajbasnet123789 <rajbasnet2027@gmail.com>
2025-11-06 09:52:53 +0000

    Intial commit

--- sfa.c
+++ sfa.c
@@ -12,12 +12,32 @@ int main() {
     int n;
     printf("Enter number of files: ");
     scanf("%d", &n);
-
+    bool block[100000]=false;
     file f[n]; 
 
     printf("Enter details: <file_no> <file_name> <start_index> <block_size>\n");
     for (int i = 0; i < n; i++) {
+        
         scanf("%d %19s %d %d", &f[i].file_no, f[i].file_name, &f[i].start_in, &f[i].block_size);
+        while(1){
+            int flag=0;
+            for(int j=f[i].start_in;j<f[i].start_in+f[i].block_size;j++){
+                if(block[j]==true){
+                    flag=1;
+                    break;
+                }
+            }
+            if(flag==1){
+                printf("Blocks from %d to %d are already allocated. Enter different start index: ",f[i].start_in,f[i].start_in+f[i].block_size-1);
+                scanf("%d",&f[i].start_in);
+            }
+            else{
+                for(int j=f[i].start_in;j<f[i].start_in+f[i].block_size;j++){
+                    block[j]=true;
+                }
+                break;
+            }
+        }
     }
 
     printf("\nFile Details:\n");
